a1de24f73870a329f835ddc09d3bfbac23568317
Honchar007 <andrey2002335@gmail.com>
2023-06-02 16:41:21 +0300

    Update root.ts

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -4,6 +4,10 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
     return { root: true }
   })
+
+  fastify.get('/asd', async function (request, reply) {
+    return { root: "hello my friend" }
+  })
 }
 
 export default root;
